1c09990a53692bc32690b7ebc9bed79032f160ee
William Benton <willb@redhat.com>
2017-03-17 16:19:13 -0500

    added preprocessor app

---
+++ app/build.sbt
@@ -0,0 +1,47 @@
+name := "rhci"
+
+organization := "com.redhat.et"
+
+version := "0.0.1"
+
+scalaVersion := "2.11.8"
+
+val SPARK_VERSION = "2.1.0"
+val SCALA_VERSION = "2.11.8"
+
+def commonSettings = Seq(
+  libraryDependencies ++= Seq(
+    "com.github.scopt" %% "scopt" % "3.5.0",
+    "org.apache.spark" %% "spark-core" % SPARK_VERSION,
+    "org.apache.spark" %% "spark-sql" % SPARK_VERSION,
+    "org.apache.spark" %% "spark-mllib" % SPARK_VERSION,
+    "org.scala-lang" % "scala-reflect" % SCALA_VERSION,
+    "org.elasticsearch" %% "elasticsearch-spark-20" % "5.1.1"
+  )
+)
+
+seq(commonSettings:_*)
+
+licenses += ("Apache-2.0", url("http://opensource.org/licenses/Apache-2.0"))
+
+scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
+
+scalacOptions in (Compile, doc) ++= Seq("-doc-root-content", baseDirectory.value+"/root-doc.txt")
+
+(dependencyClasspath in Test) <<= (dependencyClasspath in Test).map(
+  _.filterNot(_.data.name.contains("slf4j-log4j12"))
+)
+
+lazy val rhci = (project in file(".")).
+  settings(commonSettings: _*).
+  settings(
+    mainClass in assembly := Some("com.redhat.et.rhci.Main"),
+    assemblyMergeStrategy in assembly := { 
+      case m if m.toLowerCase.endsWith("manifest.mf")          => MergeStrategy.discard
+      case m if m.toLowerCase.matches("meta-inf.*\\.sf$")      => MergeStrategy.discard
+      case "log4j.properties"                                  => MergeStrategy.discard
+      case m if m.toLowerCase.startsWith("meta-inf/services/") => MergeStrategy.filterDistinctLines
+      case "reference.conf"                                    => MergeStrategy.concat
+      case _                                                   => MergeStrategy.first
+    }
+  )
---
+++ app/src/main/scala/com/redhat/et/rhci/Main.scala
@@ -0,0 +1,84 @@
+package com.redhat.et.rhci
+
+import org.apache.spark.sql.SparkSession
+import scopt.OptionParser
+
+case class PConfig(data: String="data.parquet", output: String="out.parquet", features: Int=512, partitions: Int=256, savemode: String="overwrite", master: Option[String]=None)
+
+object Main {
+  def main(args: Array[String]) {
+    args.toList match {
+      case hd::tl =>
+	hd match {
+	  case "preprocess" =>
+	    Preprocessor.main(tl.toArray)
+	  case _ =>
+	    Console.println("valid commands include \"preprocess\"")
+	}
+      case Nil =>
+	Console.println("specify a command:  valid commands include \"preprocess\"")
+    }
+  }
+}
+
+object Preprocessor {
+  def main(args: Array[String]) {
+    val parser = new OptionParser[PConfig]("") {
+      head("rhci preprocess", "0.0.1")
+
+      opt[String]("master")
+	.action((x,c) => c.copy(master=Some(x)))
+	.text("spark master to use (defaults to value of spark.master property (if set) or \"local[*]\" (if not))")
+
+      opt[String]("data")
+	.action((x,c) => c.copy(data=x))
+	.text("path of input data (defaults to \"data.parquet\")")
+
+      opt[String]('o', "output")
+	.action((x,c) => c.copy(output=x))
+	.text("destination for parquet output (defaults to \"out.parquet\")")
+
+      opt[String]("savemode")
+	.action((x,c) => c.copy(savemode=x))
+	.text("what to do if the output file already exists (default is \"overwrite\")")
+
+      opt[Int]("features")
+	.action((x,c) => c.copy(features=x))
+	.text("number of features to generate for each message")
+
+      opt[Int]("partitions")
+	.action((x,c) => c.copy(partitions=x))
+	.text("number of partitions to generate in output file")
+    }
+    
+    parser.parse(args, PConfig()) match {
+      case Some(config) =>
+	val sesh = SparkSession.builder().master(config.master.getOrElse(System.getProperty("spark.master", "local[*]"))).getOrCreate()
+	val data = config.data
+	val output = config.output
+	val savemode = config.savemode
+	val features = config.features
+	val partitions = config.partitions
+
+	val sqlc = sesh.sqlContext
+	val df = sesh.read.parquet(data)
+
+	import org.apache.spark.sql.functions._
+	import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer, StopWordsRemover}
+
+	val pro = df
+		   .filter(df("message").isNotNull)
+		   .select(df("ci_job.build_id"), when(df("ci_job.status") === "SUCCESS", 0).otherwise(1).alias("status"), lower(df("message")).alias("message"))
+	
+	val tokenizer = new Tokenizer().setInputCol("message").setOutputCol("rawTokens")
+	val remover = new StopWordsRemover().setInputCol("rawTokens").setOutputCol("tokens")
+	val htf = new HashingTF().setNumFeatures(128).setInputCol("tokens").setOutputCol("features")
+	val hashed = htf.transform(remover.transform(tokenizer.transform(pro)))
+
+	hashed.select("build_id", "status", "message", "features").write.mode(savemode).save(output)
+
+	sesh.sparkContext.stop
+      case None => ()
+    }
+  }
+}
