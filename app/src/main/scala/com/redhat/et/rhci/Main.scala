9355165cfd33056f86ea434c376c10af1089744e
William Benton <willb@redhat.com>
2017-03-21 09:48:41 -0500

    added SOM app skeleton

--- app/src/main/scala/com/redhat/et/rhci/Main.scala
+++ app/src/main/scala/com/redhat/et/rhci/Main.scala
@@ -106,7 +106,7 @@ object Afforest {
 
       opt[String]('o', "output")
 	.action((x,c) => c.copy(output=x))
-	.text("destination for parquet output (defaults to \"forest.model\")")
+	.text("destination for serialized model output (defaults to \"forest.model\")")
 
       opt[String]("savemode")
 	.action((x,c) => c.copy(savemode=x))
@@ -135,8 +135,70 @@ object Afforest {
 	val Array(training, test) = df.randomSplit(Array(trainSample, 1.0d - trainSample))
 	val model = rf.fit(training)
 	
+	model.save(output)
+      
 	sesh.sparkContext.stop
       case None => ()
     }
   }
+}
+
+case class OConfig(data: String="out.parquet", output: String="som", savemode: String="overwrite", master: Option[String]=None, xdim: Int=10, ydim: Int=10)
+
+object Organize {
+  def main(args: Array[String]) {
+    val parser = new OptionParser[OConfig]("") {
+      head("rhci organize", "0.0.1")
+
+      opt[String]("master")
+	.action((x,c) => c.copy(master=Some(x)))
+	.text("spark master to use (defaults to value of spark.master property (if set) or \"local[*]\" (if not))")
+
+      opt[String]("data")
+	.action((x,c) => c.copy(data=x))
+	.text("path of preprocessed input data (defaults to \"out.parquet\")")
+
+      opt[String]('o', "output")
+	.action((x,c) => c.copy(output=x))
+	.text("prefix for model output (defaults to \"output\")")
+
+      opt[Int]('x', "xdim")
+	.action((x,c) => c.copy(xdim=x))
+	.text("width of map (defaults to 10)")
+
+      opt[Int]('y', "ydim")
+	.action((x,c) => c.copy(ydim=x))
+	.text("height of map (defaults to 10)")
+    }
+    
+    parser.parse(args, OConfig()) match {
+      case Some(config) =>
+	val sesh = SparkSession.builder().master(config.master.getOrElse(System.getProperty("spark.master", "local[*]"))).getOrCreate()
+	val data = config.data
+	val output = config.output
+	val xdim = config.xdim
+	val ydim = config.ydim
+	
+	val sqlc = sesh.sqlContext
+	val df = sesh.read.parquet(data)
+
+	import org.apache.spark.sql.Row
+	import org.apache.spark.sql.functions._
+	import org.apache.spark.mllib.linalg.{Vector => SV}
+
+	import com.redhat.et.silex.som.SOM
+
+	val successes = df.filter(df("status") === 0).select("features").rdd.map { case Row(s: SV) => s }
+	val failures = df.filter(df("status") === 1).select("features").rdd.map { case Row(s: SV) => s }
+	val fdim = (successes.take(1))(0).size
+	val successmap = SOM.train(xdim, ydim, fdim, 20, successes, sigmaScale=0.7)
+	val failuremap = SOM.train(xdim, ydim, fdim, 20, failures, sigmaScale=0.7)
+
+	SOM.save(successmap, s"$output-successes.som")
+	SOM.save(failuremap, s"$output-failures.som")
+      
+	sesh.sparkContext.stop
+      case None => ()
+    }
+  }  
 }
